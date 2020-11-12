241789621f300e2924da6b12a608a77bb030bea2
Thiago Bernardo <thiagorbernardo@hotmail.com>
2020-11-12 11:57:40 -0300

    Using animations

--- src/components/Alarm.tsx
+++ src/components/Alarm.tsx
@@ -9,81 +9,66 @@ import {
 import LinearGradient from 'react-native-linear-gradient';
 import {IconButton} from 'react-native-paper';
 import Medicine from '../models/Medicine.model';
-
-const alarmCardColors = [
-  '#f2e0dc',
-  '#f2e0dc',
-  '#f2e0dc',
-  '#f2e0dc',
-  '#f2e0dc',
-  '#f0d5d1',
-  '#f0d5d1',
-  '#f0d5d1',
-  '#f0d5d1',
-  '#f0d5d1',
-  '#e6c8c3',
-  '#e6c8c3',
-  '#e6c8c3',
-  '#e6c8c3',
-  '#e8c6c1',
-  '#e8c6c1',
-  '#ebc9c3',
-  '#ebc9c3',
-];
+import {AppTheme} from '../theme/App.theme';
+import * as Animatable from 'react-native-animatable';
+import LottieView from 'lottie-react-native';
 
 export default function Alarm(med: Medicine) {
   const [alarmPressed, setAlarmPressed] = useState(false);
 
   return (
     <>
-      <LinearGradient
-        start={{
-          x: 0.3,
-          y: 0.1,
-        }}
-        end={{
-          x: 1,
-          y: 1,
-        }}
-        colors={alarmCardColors}
-        style={styles.alarmView}>
-        <TouchableWithoutFeedback
-          onPress={() => {
-            setAlarmPressed(!alarmPressed);
-            //TODO:Add height extended in view
-          }}>
-          <View style={styles.rowItens}>
-            <View style={styles.imgColumn}>
-              <Image
-                source={require('../assets/img/pill.png')}
-                style={styles.tinyLogo}
-              />
-            </View>
-            <View style={styles.columnItens}>
-              <View style={styles.titles}>
-                <Text style={styles.alarmTitle}>{med.name}</Text>
-                <Text style={styles.alarmSubTitle}>{med.specs}</Text>
+      <Animatable.View
+        animation="slideInDown"
+        iterationCount={1}
+        direction="alternate">
+        <View>
+          <LinearGradient
+            start={{x: 0.3, y: 0.1}}
+            end={{x: 1, y: 1}}
+            colors={AppTheme.alarmCardColors}
+            style={alarmPressed ? styles.alarmViewPressed : styles.alarmView}>
+            <TouchableWithoutFeedback
+              onPress={() => {
+                setAlarmPressed(!alarmPressed);
+              }}>
+              <View>
+                <View style={styles.rowItens}>
+                  <View style={styles.imgColumn}>
+                    <Image
+                      source={require('../assets/img/pill.png')}
+                      style={styles.tinyLogo}
+                    />
+                  </View>
+                  <View style={styles.columnItens}>
+                    <View style={styles.titles}>
+                      <Text style={styles.alarmTitle}>{med.name}</Text>
+                      <Text style={styles.alarmSubTitle}>{med.specs}</Text>
+                    </View>
+                    <View style={styles.clock}>
+                      <Text style={styles.clockText}>{med.hours}</Text>
+                    </View>
+                  </View>
+                  <View style={styles.icon}>
+                    {alarmPressed ? (
+                      <IconButton
+                        size={40}
+                        color="#1A2D4E"
+                        icon="chevron-down"></IconButton>
+                    ) : (
+                      <IconButton
+                        size={40}
+                        color="#1A2D4E"
+                        icon="chevron-right"></IconButton>
+                    )}
+                  </View>
+                </View>
+                {alarmPressed ? <Text>BLA BLA BLA</Text> : <></>}
               </View>
-              <View style={styles.clock}>
-                <Text style={styles.clockText}>{med.hours}</Text>
-              </View>
-            </View>
-            <View style={styles.icon}>
-              {alarmPressed ? (
-                <IconButton
-                  size={40}
-                  color="#1A2D4E"
-                  icon="chevron-down"></IconButton>
-              ) : (
-                <IconButton
-                  size={40}
-                  color="#1A2D4E"
-                  icon="chevron-right"></IconButton>
-              )}
-            </View>
-          </View>
-        </TouchableWithoutFeedback>
-      </LinearGradient>
+            </TouchableWithoutFeedback>
+          </LinearGradient>
+        </View>
+      </Animatable.View>
     </>
   );
 }
@@ -103,8 +88,15 @@ const styles = StyleSheet.create({
   alarmView: {
     borderRadius: 30,
     padding: 10,
-    margin: 20,
+    margin: 18,
     height: 120,
+    elevation: 12,
+  },
+  alarmViewPressed: {
+    borderRadius: 30,
+    padding: 10,
+    margin: 20,
+    height: 300,
     elevation: 12,
   },
   rowItens: {
