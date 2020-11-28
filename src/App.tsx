98f284bdefd661582b644914be93321b7e2dc16a
Thiago Ramos <thiagorbernardo@hotmail.com>
2020-11-28 19:21:00 -0300

    Using AsyncStorage to store first time using the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {SafeAreaView, StyleSheet, StatusBar} from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import TopBar from './components/TopBar';
 import HomeScreen from './screens/HomeScreen';
+import {
+  setStorageData,
+  getStorageData,
+  deleteStorageData,
+} from './services/AsyncStorageController';
 // import OnboardingScreen from './OnboardingScreen';
 
 const App = () => {
+  getStorageData('firstTimeUsingApp').then((isNewUser) => {
+    console.log('Test:', isNewUser);
+    if(isNewUser == null)
+      setStorageData('firstTimeUsingApp', true);
+  });
+  deleteStorageData('firstTimeUsingApp')
   return (
     <>
       <StatusBar backgroundColor="#FAEFED" barStyle="dark-content" />
