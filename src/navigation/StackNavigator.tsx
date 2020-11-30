bce235d64993fef90cf00e7533cd7b338b6e37f6
Thiago Ramos <thiagorbernardo@hotmail.com>
2020-11-29 23:20:20 -0300

    Implementing login screen boilerplate

--- src/navigation/StackNavigator.tsx
+++ src/navigation/StackNavigator.tsx
@@ -4,15 +4,17 @@ import {createStackNavigator} from '@react-navigation/stack';
 import HomeScreen from '../screens/HomeScreen';
 import OnboardingScreen from '../screens/OnboardingScreen';
 import LoadingScreen from '../components/LoadingScreen';
+import LoginScreen from '../screens/LoginScreen';
 
 const Stack = createStackNavigator();
 
 export default function StackNavigator() {
   return (
-    <Stack.Navigator headerMode="none" initialRouteName="Onboarding">
+    <Stack.Navigator headerMode="none" initialRouteName="Login">
       <Stack.Screen name="Home" component={HomeScreen} />
       <Stack.Screen name="Onboarding" component={OnboardingScreen} />
       <Stack.Screen name="Loading" component={LoadingScreen} />
+      <Stack.Screen name="Login" component={LoginScreen} />
     </Stack.Navigator>
   );
 }
