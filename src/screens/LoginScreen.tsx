bd0cd7a0abc232fee5d3124f1d334579ca4e2cbe
Thiago Ramos <thiagorbernardo@hotmail.com>
2020-11-30 01:22:14 -0300

    Login Screen

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,84 +1,109 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+import React, {useState} from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
+import LottieView from 'lottie-react-native';
+import {AppTheme} from '../theme/App.theme';
 
 export default function LoginScreen() {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-    return (
-      <View style={styles.container}>
-        <Text style={styles.logo}>HeyAPP</Text>
-        <View style={styles.inputView} >
-          <TextInput  
-            style={styles.inputText}
-            placeholder="Email..." 
-            placeholderTextColor="#003f5c"
-            onChangeText={text => setEmail(text)}/>
-        </View>
-        <View style={styles.inputView} >
-          <TextInput  
-            secureTextEntry
-            style={styles.inputText}
-            placeholder="Password..." 
-            placeholderTextColor="#003f5c"
-            onChangeText={text => setPassword(text)}/>
-        </View>
-        <TouchableOpacity>
-          <Text style={styles.forgot}>Forgot Password?</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.loginBtn}>
-          <Text style={styles.loginText}>LOGIN</Text>
-        </TouchableOpacity>
-        <TouchableOpacity>
-          <Text style={styles.loginText}>Signup</Text>
-        </TouchableOpacity>
-
-  
+  const onPressSend = () => {
+    console.log(email, password);
+  };
+  return (
+    <LinearGradient
+      colors={AppTheme.backgroundGradientHome}
+      style={styles.container}>
+      <Text style={styles.logo}>Rim Saudável</Text>
+      <LottieView
+        source={require('../assets/lottie/woman-meditation.json')}
+        autoPlay
+        resizeMode="contain"
+        style={styles.lottie}
+      />
+      <View style={styles.inputView}>
+        <TextInput
+          style={styles.inputText}
+          placeholder="Email..."
+          placeholderTextColor="#003f5c"
+          onChangeText={(text) => setEmail(text)}
+        />
       </View>
-    );
+      <View style={styles.inputView}>
+        <TextInput
+          secureTextEntry
+          style={styles.inputText}
+          placeholder="Senha..."
+          placeholderTextColor="#003f5c"
+          onChangeText={(text) => setPassword(text)}
+        />
+      </View>
+      <TouchableOpacity>
+        <Text style={styles.forgot}>Esqueceu a Senha?</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.loginBtn} onPress={onPressSend}>
+        <Text style={styles.loginText}>ENTRAR</Text>
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <Text style={styles.loginText}>Cadastre-se</Text>
+      </TouchableOpacity>
+    </LinearGradient>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#003f5c',
     alignItems: 'center',
     justifyContent: 'center',
   },
-  logo:{
-    fontWeight:"bold",
-    fontSize:50,
-    color:"#fb5b5a",
-    marginBottom:40
+  logo: {
+    fontWeight: 'bold',
+    fontSize: 50,
+    color: '#465C69',
+    marginBottom: 40,
   },
-  inputView:{
-    width:"80%",
-    backgroundColor:"#465881",
-    borderRadius:25,
-    height:50,
-    marginBottom:20,
-    justifyContent:"center",
-    padding:20
+  inputView: {
+    width: '80%',
+    backgroundColor: 'white',
+    borderRadius: 25,
+    height: 50,
+    marginBottom: 20,
+    justifyContent: 'center',
+    padding: 20,
   },
-  inputText:{
-    height:50,
-    color:"white"
+  inputText: {
+    height: 50,
+    color: 'black',
   },
-  forgot:{
-    color:"white",
-    fontSize:11
+  forgot: {
+    color: 'black',
+    fontSize: 13,
   },
-  loginBtn:{
-    width:"80%",
-    backgroundColor:"#fb5b5a",
-    borderRadius:25,
-    height:50,
-    alignItems:"center",
-    justifyContent:"center",
-    marginTop:40,
-    marginBottom:10
+  loginBtn: {
+    width: '80%',
+    backgroundColor: '#498467',
+    borderRadius: 25,
+    height: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 40,
+    marginBottom: 10,
   },
-  loginText:{
-    color:"white"
-  }
+  loginText: {
+    color: 'white',
+  },
+  lottie: {
+    width: 270,
+    height: 270,
+    justifyContent: 'flex-end',
+    marginBottom: 10
+  },
 });
